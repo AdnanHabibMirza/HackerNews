3bea931f5dd432503a5c6bed887589d97c614929
Adnan Habib <contactadnanhabib@gmail.com>
2023-08-25 14:46:35 +0500

    Create project

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Adnan Habib on 25/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
